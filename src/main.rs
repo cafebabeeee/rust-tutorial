ed977e9973a94f3edb79c84276ad49dba14f5c43
CAFEBABE <neuropathycc@gmail.com>
2020-11-30 10:38:03 +0800

    CHANGE MODULES

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod borrow;
-mod ownership;
-mod lifetime;
 mod function;
-//mod smartptr_ownership;
+mod lifetime;
+mod ownership;
 
 fn main() {
-
+    let a = 15;
+    let mut b = 3;
+    borrow::borrow::compute(&a, &mut b);
+    println!(" b value is {}", b)
 }
